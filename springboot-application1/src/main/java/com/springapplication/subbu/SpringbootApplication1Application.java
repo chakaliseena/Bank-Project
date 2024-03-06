960f8a95c4e55447c53a726a47b04ea86a5f535b
chakaliseena <162320661+chakaliseena@users.noreply.github.com>
2024-03-06 10:56:33 +0530

    Add files via upload

---
+++ springboot-application1/src/main/java/com/springapplication/subbu/SpringbootApplication1Application.java
@@ -0,0 +1,13 @@
+package com.springapplication.subbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication1Application.class, args);
+	}
+
+}
---
+++ springboot-application1/src/test/java/com/springapplication/subbu/SpringbootApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapplication.subbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
